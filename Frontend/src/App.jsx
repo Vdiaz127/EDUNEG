08df0c296b420c032e55364634f3a697af05b963
Carlos Cesin <carlos@softcode.es>
2025-02-18 20:21:51 -0400

    Login implementation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/login/login'; 
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,13 +19,7 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">Bienvenido a EDUNEG</h1>
-      <p className="text-lg text-gray-700 mb-6">
-        Aquí puedes comprar el uso de nuestros servicios de educación en línea.
-      </p>
-      <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
-        Comprar Ahora [test]
-      </button>
+      <Login/>
     </div>
   );
 }
