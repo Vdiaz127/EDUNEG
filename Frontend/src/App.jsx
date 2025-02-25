289013e24d6dc422fc8e7b853e9a6e69096029fe
DionnerFigueras <dionnerfigueras@gmail.com>
2025-02-24 20:12:19 -0400

    Agergando plan de evaluacion

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Importa los componentes de página
 
-import AdminPage from './pages/Administrador/page';
-import EstudiantesPage from './pages/Administrador/Estudiantes/page';
-import FormularioEstudiante from './pages/Administrador/Estudiantes/AgregarEstudiante/page';
-import VerEstudiante from './pages/Administrador/Estudiantes/VerEstudiante/page';
-import MateriasPage from './pages/Administrador/Materias/page';
-import FormularioMateria from './pages/Administrador/Materias/AgregarMateria/page';
-import VerMateria from './pages/Administrador/Materias/VerMateria/page';
-import ProfesoresPage from './pages/Administrador/Profesores/page';
-import FormularioProfesor from './pages/Administrador/Profesores/AgregarProfesor/page';
-import VerProfesor from './pages/Administrador/Profesores/VerProfesor/page';
+import AdminPage from "./pages/Administrador/page";
+import EstudiantesPage from "./pages/Administrador/Estudiantes/page";
+import FormularioEstudiante from "./pages/Administrador/Estudiantes/AgregarEstudiante/page";
+import VerEstudiante from "./pages/Administrador/Estudiantes/VerEstudiante/page";
+import MateriasPage from "./pages/Administrador/Materias/page";
+import FormularioMateria from "./pages/Administrador/Materias/AgregarMateria/page";
+import VerMateria from "./pages/Administrador/Materias/VerMateria/page";
+import ProfesoresPage from "./pages/Administrador/Profesores/page";
+import FormularioProfesor from "./pages/Administrador/Profesores/AgregarProfesor/page";
+import VerProfesor from "./pages/Administrador/Profesores/VerProfesor/page";
+import PlanDeEvaluacion from "./pages/Profesor/page";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Rutas del Administrador */}
-        <Route path="/administrador" element={<AdminPage />} >
+        <Route path="/administrador" element={<AdminPage />}>
           {/* Rutas de estudiantes */}
           <Route path="estudiantes" element={<EstudiantesPage />} />
-          <Route path="estudiantes/agregar" element={<FormularioEstudiante />} />
-          <Route path="estudiantes/ver/:id" element={<VerEstudiante />} /> 
+          <Route
+            path="estudiantes/agregar"
+            element={<FormularioEstudiante />}
+          />
+          <Route path="estudiantes/ver/:id" element={<VerEstudiante />} />
           {/* Rutas de materias */}
           <Route path="materias" element={<MateriasPage />} />
           <Route path="materias/agregar" element={<FormularioMateria />} />
@@ -33,8 +37,11 @@ function App() {
           <Route path="profesores/ver/:id" element={<VerProfesor />} />
         </Route>
 
+        <Route path="/profesor" element={<PlanDeEvaluacion />}></Route>
       </Routes>
     </BrowserRouter>
+
+    
   );
 }
 
