0fd078c7ccc32b3d08f73a71996e26ad9ce53fa8
DionnerFigueras <dionnerfigueras@gmail.com>
2025-02-25 20:29:04 -0400

    Resolviendo confictos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Importa los componentes de página
-
-import AdminPage from "./pages/Administrador/page";
-import EstudiantesPage from "./pages/Administrador/Estudiantes/page";
-import FormularioEstudiante from "./pages/Administrador/Estudiantes/AgregarEstudiante/page";
-import VerEstudiante from "./pages/Administrador/Estudiantes/VerEstudiante/page";
-import MateriasPage from "./pages/Administrador/Materias/page";
-import FormularioMateria from "./pages/Administrador/Materias/AgregarMateria/page";
-import VerMateria from "./pages/Administrador/Materias/VerMateria/page";
-import ProfesoresPage from "./pages/Administrador/Profesores/page";
-import FormularioProfesor from "./pages/Administrador/Profesores/AgregarProfesor/page";
-import VerProfesor from "./pages/Administrador/Profesores/VerProfesor/page";
-import PlanDeEvaluacion from "./pages/Profesor/page";
-
 // src/App.js
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -49,45 +33,11 @@ import EditarSemestre from './pages/Administrador/Semestres/EditarSemestre';
 
 
 import Login from './components/login/login'; 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// Importa los componentes de página
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// Importa los componentes de página
-
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// Importa los componentes de página
-
-import AdminPage from './pages/Administrador/page';
-import EstudiantesPage from './pages/Administrador/Estudiantes/page';
-import FormularioEstudiante from './pages/Administrador/Estudiantes/AgregarEstudiante/page';
-import VerEstudiante from './pages/Administrador/Estudiantes/VerEstudiante/page';
-import MateriasPage from './pages/Administrador/Materias/page';
-import FormularioMateria from './pages/Administrador/Materias/AgregarMateria/page';
-import VerMateria from './pages/Administrador/Materias/VerMateria/page';
-import ProfesoresPage from './pages/Administrador/Profesores/page';
-import FormularioProfesor from './pages/Administrador/Profesores/AgregarProfesor/page';
-import VerProfesor from './pages/Administrador/Profesores/VerProfesor/page';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Rutas del Administrador */}
-        <Route path="/administrador" element={<AdminPage />} >
-          {/* Rutas de estudiantes */}
-          <Route path="estudiantes" element={<EstudiantesPage />} />
-          <Route
-            path="estudiantes/agregar"
-            element={<FormularioEstudiante />}
-          />
-          <Route path="estudiantes/ver/:id" element={<VerEstudiante />} />
-
         {/* Todas las rutas de /administrador usarán el AdminLayout */}
         <Route path="/administrador" element={<AdminLayout />}>
           {/* Página principal de administrador */}
@@ -97,32 +47,15 @@ function App() {
           <Route path="estudiantes/agregar" element={<FormularioEstudiante />} />
           <Route path="estudiantes/ver/:id" element={<VerEstudiante />} />
           <Route path="estudiantes/editar/:id" element={<EditarEstudiante />} />
-
-          <Route path="estudiantes/agregar" element={<FormularioEstudiante />} />
-          <Route path="estudiantes/ver/:id" element={<VerEstudiante />} /> 
-
-          <Route path="estudiantes/agregar" element={<FormularioEstudiante />} />
-          <Route path="estudiantes/ver/:id" element={<VerEstudiante />} /> 
-
-          <Route path="estudiantes/agregar" element={<FormularioEstudiante />} />
-          <Route path="estudiantes/ver/:id" element={<VerEstudiante />} /> 
-
           {/* Rutas de materias */}
           <Route path="materias" element={<MateriasPage />} />
           <Route path="materias/agregar" element={<FormularioMateria />} />
           <Route path="materias/ver/:id" element={<VerMateria />} />
-
           <Route path="materias/editar/:id" element={<EditarMateria />} />
-
           {/* Rutas de profesores */}
           <Route path="profesores" element={<ProfesoresPage />} />
           <Route path="profesores/agregar" element={<FormularioProfesor />} />
           <Route path="profesores/ver/:id" element={<VerProfesor />} />
-
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
           <Route path="profesores/editar/:id" element={<EditarProfesor />} />
 
           <Route path="secciones" element={<SeccionesPage />} />
